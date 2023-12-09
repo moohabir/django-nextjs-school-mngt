67df585249d6b7d69eab363fc67029fd738ec247
moohabir <moohabir@gmail.com>
2023-12-09 02:21:40 +0100

    latest

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -65,6 +65,7 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "https://django-next-frontend-1h6a769a8-moohabir.vercel.app",
+    "https://django-next-frontend.vercel.app"
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
